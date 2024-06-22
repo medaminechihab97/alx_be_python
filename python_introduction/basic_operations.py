db5d5cae6160d759b2165ea00d7ae0139be9378f
medaminechihab97 <med.chihab97@gmail.com>
2024-06-22 16:46:54 +0100

    created basic operation script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+num1 = 5
+num2 = 10
+mysum = num1 + num2
+sub = num2 - num1
+mult = num1 * num2
+print ("addition of ", num1, "and", num2, " is", mysum)
+print ("substraction of ", num1, "and", num2, " is", sub)
+print ("multiplication of ", num1, "and", num2, " is", mult)
