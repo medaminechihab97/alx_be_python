4aa0e995f18236c7fce8d2795b3b26e602983d9b
medaminechihab97 <med.chihab97@gmail.com>
2024-06-30 21:39:49 +0100

    created a match case calculator

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,18 +4,18 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
     case "low":
         if time_bound == "yes":
-            reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
     case "medium":
         if time_bound == "yes":
-            reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
 
-print (reminder)
+print (Reminder)
