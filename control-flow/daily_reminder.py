73162adc0ffa87b79305608cca67dc8ed881fe9a
medaminechihab97 <med.chihab97@gmail.com>
2024-06-30 21:37:26 +0100

    created a match case calculator

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,8 +1,21 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
-match time_bound:
-    case "yes":
-        print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-    case "no":
-        print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+match priority:
+    case "high":
+        if time_bound == "yes":
+            note =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            note =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+    case "low":
+        if time_bound == "yes":
+            note =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            note =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+    case "medium":
+        if time_bound == "yes":
+            note =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            note =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+
+print (note)
