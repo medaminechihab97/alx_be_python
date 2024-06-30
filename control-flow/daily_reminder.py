ca098ab4384a3bec55f8c37d74a3dd8a1cb7fd65
medaminechihab97 <med.chihab97@gmail.com>
2024-06-30 21:48:09 +0100

    created a match case calculator

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,18 +4,21 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
     case "low":
         if time_bound == "yes":
-            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
     case "medium":
         if time_bound == "yes":
-            Reminder =(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            Reminder =(f"note: '{task}' is a {priority} priority task that requires immediate attention today!")
-print("Reminder: ")
+            Reminder =(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+if priority == "high":
+    print("Reminder: ", end="")
+else :
+    print("Note: ", end="")
 print (Reminder)
