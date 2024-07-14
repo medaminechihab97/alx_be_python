33a2203de2e1e7ff7d938964f28cab354760874f
medaminechihab97 <med.chihab97@gmail.com>
2024-07-14 23:17:52 +0100

    created a bank account app using oop and command line interface

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,7 +5,7 @@ import unittest
 class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         result = SimpleCalculator.add(5, 3)
-        self.assertEqual (self.calc.add) 
+        self.assertEqual(self.calc.add)
     def test_subtract_positive(self):
         result = SimpleCalculator.subtract(5, 3)
         self.assertEqual(result, 2) 
