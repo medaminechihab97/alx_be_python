7b1494c40501c75458302c51f80eb3b7a0b30549
medaminechihab97 <med.chihab97@gmail.com>
2024-07-14 23:12:04 +0100

    created a bank account app using oop and command line interface

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,17 @@
+from simple_calculator import SimpleCalculator 
+import unittest
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def test_add_positive(self):
+        result = SimpleCalculator.add(5, 3)
+        self.assertEqual(result, 8) 
+    def test_subtract_positive(self):
+        result = SimpleCalculator.subtract(5, 3)
+        self.assertEqual(result, 2) 
+    def test_multiplication_positive(self):
+        result = SimpleCalculator.multiply(5, 3)
+        self.assertEqual(result, 15)
+    def test_division_positive(self, a, b):
+        assert b != 0, "Error: Division by zero!"
+        
