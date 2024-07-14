73b5df5f5af6e51138f54ec3aedf2c5beef81bd1
medaminechihab97 <med.chihab97@gmail.com>
2024-07-14 22:49:11 +0100

    created a bank account app using oop and command line interface

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,9 +5,10 @@ def safe_divide(numerator, denominator):
         float(numerator)
         float(denominator)
         division = numerator / denominator
+        print(f"The result of the division is {division}")
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
         print("Error: Please enter numeric values only.")
-        
+
 
