aed6ba89c3c47232acd855e306a1d9a94f1ed470
medaminechihab97 <med.chihab97@gmail.com>
2024-07-14 22:50:34 +0100

    created a bank account app using oop and command line interface

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,5 @@
 def safe_divide(numerator, denominator):
     try:
-        numerator = input("Enter the numerator: ")
-        denominator = input("Enter the denominator: ")
         float(numerator)
         float(denominator)
         division = numerator / denominator
