ea403c8ab769a810b182334532efe7646f6b81fe
medaminechihab97 <med.chihab97@gmail.com>
2024-07-14 21:55:54 +0100

    created a bank account app using oop and command line interface

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
             return False
         
     def display_balance (self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}.00")
     
 
         
