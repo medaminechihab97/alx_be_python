10557228401e21c9640aa716337323adb1440627
medaminechihab97 <med.chihab97@gmail.com>
2024-07-21 16:21:59 +0100

    created a book class that uses magic methds

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,1 +1,10 @@
-import mailbox
+class Book:
+    def __init__ (self):
+        pass
+    def __str__ (self):
+        pass
+    def __repr__(self):
+        pass
+    def __del__(self):
+        pass
+    
