c3ef1e653310d736cc12d80a62c565c2bbfb7a02
medaminechihab97 <med.chihab97@gmail.com>
2024-07-21 16:23:51 +0100

    created a book class that uses magic methds

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,10 +1,11 @@
 class Book:
-    def __init__ (self):
-        pass
+    def __init__ (self, title , author, year):
+        self.title = title
+        self.author = author
+        self.year = year
     def __str__ (self):
-        pass
+        return(f"{self.title} by {self.author}, published in {self.year}")
     def __repr__(self):
-        pass
+        return(f"Book('{self.title}', '{self.author}', {self.year})")
     def __del__(self):
-        pass
-    
+        print(f"Deleting {self.title}")
