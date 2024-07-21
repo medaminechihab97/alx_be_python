d371c6027b945bf4136336a2b6624c343755f4ee
medaminechihab97 <med.chihab97@gmail.com>
2024-07-21 15:17:40 +0100

    created a bank account app using oop and command line interface

--- programming_paradigms/exceptions.py
+++
@@ -1,8 +0,0 @@
-def division(x , y):
-    if y == 0:
-        raise ZeroDivisionError("you can not devide by zero!")
-    print( x / y)
-
-x = int(input("Enter the number you want to devide: "))
-y = int(input("Enter the number you want to devide by: "))
-division(x , y)
