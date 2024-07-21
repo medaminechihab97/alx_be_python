d5258b001b59b2b07577aed4a5908c598c0388f6
medaminechihab97 <med.chihab97@gmail.com>
2024-07-21 22:23:33 +0100

    created a calculation app with static and class methods

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,6 +1,7 @@
 class Calculator:
-    def __init__ (self):
-        self.calculation_type = "Arithmetic Operations"
+    # def __init__ (self):
+    #     self.
+    calculation_type = "Arithmetic Operations"
     @staticmethod
     def add(a, b):
         return a + b
@@ -9,3 +10,5 @@ class Calculator:
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
+    
+
